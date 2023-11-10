a5d56905f3e77218861037d0bf3f4601e7e6b24b
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-11-10 14:23:00 +0100

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1200,
+      once: false,
+    });
+  }
 }
+
